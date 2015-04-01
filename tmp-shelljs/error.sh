9516ecb25250877b6bcb49c0f005f4c3f7c4b7b9
Dominykas Blyžė <hello@dominykas.com>
2015-04-01 15:41:12 +0300

    echo to stderr?

--- tmp-shelljs/error.sh
+++ tmp-shelljs/error.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-echo "fatal: ambiguous argument 'HEAD': unknown revision or path not in the working tree."
-echo "Use '--' to separate paths from revisions, like this:"
-echo "'git <command> [<revision>...] -- [<file>...]'"
-echo "HEAD"
+echo "fatal: ambiguous argument 'HEAD': unknown revision or path not in the working tree." 1>&2
+echo "Use '--' to separate paths from revisions, like this:" 1>&2
+echo "'git <command> [<revision>...] -- [<file>...]'" 1>&2
+echo "HEAD" 1>&2
 exit 1
